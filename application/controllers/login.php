cdc327a7213d331945de43d88c0261c80a435dd6
hanscarreon <hanscarreon0898@gmail.com>
2020-10-16 11:35:12 +0800

    add

--- application/controllers/login.php
+++ application/controllers/login.php
@@ -7,7 +7,7 @@ class Login extends CI_Controller {
 	public function __construct()
 	{
 		parent::__construct();
-		$this->load->database();
+		// $this->load->database();
 		$this->load->helper(array('url','date', 'form'));
 		$this->load->library(array('form_validation', 'session', 'pagination', 'uuid'));
 		$this->load->model('model_base');
--- application/views/view_login.php
+++ application/views/view_login.php
@@ -13,7 +13,7 @@
   <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
   <!-- icheck bootstrap -->
   <link rel="stylesheet" href="<?php echo base_url() ?>plugins/icheck-bootstrap/icheck-bootstrap.min.css">
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+  <link rel="stylesheet" href="<?php echo base_url() ?>resources/bs/bs4.css" >
 
   <link rel="stylesheet" href="<?php echo base_url(); ?>plugins/toastr/toastr.min.css">
 
@@ -210,9 +210,9 @@
 
 
 <!-- Bootstrap 4 -->
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+<script src="<?php echo base_url() ?>resources/bs/3_5_1.js"></script>
+<script src="<?php echo base_url() ?>resources/bs/popper.js" ></script>
+<script src="<?php echo base_url() ?>resources/bs/bs.js"></script>
 
 
 
